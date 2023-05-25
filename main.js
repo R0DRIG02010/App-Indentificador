6c38d02e564fbb59cc0d28efd28eac641ec704c3
R0DRIG02010 <108089716+R0DRIG02010@users.noreply.github.com>
2023-05-25 14:31:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet',modelLoadead);
+}
+
+function draw() {
+  image(video,0, 0, 300, 300);
+  classifier.classify(video, gotResult);
+}
+
+function modelLoadead() {
+  console.log('Model Loaded');
+}
+var previousResult = '';
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    if((results[0].confidence > 0.5) && (previousResult != results[0].label )){
+      console.log(results);
+      previousResult = results[0].label;
+      var synth = windows.speehSynthesis;
+      seakData ='O objeto dectado é -'+results[0].label;
+      var utterThis = new SpeechSynthesisUtterance(speakData);
+      synth.speak(utterThis);
+
+      document.getElementById9("resultObjectName").innerHTML = results[0].label;
+      document.getElementById("resultObectAccuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+  }
+}
